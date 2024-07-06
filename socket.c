6ea5696b2d5d3471dd3638286c7a713a05e09d3e
faycarsons <faycarsons23@gmail.com>
2024-07-06 08:27:28 -0400

    add ability to create client, for requests

--- socket.c
+++ socket.c
@@ -7,11 +7,12 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <arpa/inet.h>
 
 #define DEBUG false
 
 // String must be borrowed!
-const char *kk_string_to_buf(kk_string_t s, kk_context_t *ctx)
+const char *kk_string_to_buf(const kk_string_t s, kk_context_t *ctx)
 {
   const kk_bytes_t bytes = s.bytes;
   kk_ssize_t len = kk_bytes_len_borrow(bytes, ctx);
@@ -25,7 +26,7 @@ const char *kk_string_to_buf(kk_string_t s, kk_context_t *ctx)
 #endif
 }
 
-kk_std_core_exn__error kk_socket(int domain, int socktype, kk_context_t *ctx)
+kk_std_core_exn__error kk_socket(const int domain, const int socktype, kk_context_t *ctx)
 {
   int sockfd = socket(domain, socktype, 0);
 
@@ -39,32 +40,44 @@ kk_std_core_exn__error kk_socket(int domain, int socktype, kk_context_t *ctx)
   }
 }
 
-intptr_t new_sockaddr(int family, int addr_type, int port)
+intptr_t host_addr(const int domain, const int addr_type, const int port)
 {
   struct sockaddr_in *addr = malloc(sizeof(struct sockaddr_in));
+  if (addr == NULL) return (intptr_t)-1;
 
-  addr->sin_family = family;
+  addr->sin_family = domain;
   addr->sin_addr.s_addr = htonl(addr_type);
   addr->sin_port = htons(port);
   return (intptr_t)addr;
 }
 
+intptr_t kk_client_addr(const int domain, const kk_string_t ip, const int port, kk_context_t *ctx)
+{
+  struct sockaddr_in *host_addr = malloc(sizeof(struct sockaddr_in));
+  host_addr->sin_family = domain;
+  host_addr->sin_port = htons(port);
+
+  const char *ip_s = kk_string_to_buf(ip, ctx);
+  int try = inet_pton(domain, ip_s, &host_addr->sin_addr);
+  return (intptr_t)(try < 0 ? try : host_addr);
+}
+
 // Connect and bind must be wrapped so that we can handle casting an intptr_t
 // to a sockaddr
 
-int do_bind(int sock, intptr_t addr_)
+int do_bind(const int sock, const intptr_t addr_)
 {
   struct sockaddr *addr = (struct sockaddr *)addr_;
   return bind(sock, addr, sizeof(struct sockaddr));
 }
 
-int do_connect(int sock, intptr_t addr_)
+int do_connect(const int sock, const intptr_t addr_)
 {
   struct sockaddr *addr = (struct sockaddr *)addr_;
   return connect(sock, addr, sizeof(*addr));
 }
 
-kk_std_core_exn__error kk_accept(int sock, kk_context_t *ctx)
+kk_std_core_exn__error kk_accept(const int sock, kk_context_t *ctx)
 {
   struct sockaddr_in client_addr;
   socklen_t client_len = sizeof(client_addr);
@@ -79,10 +92,8 @@ kk_std_core_exn__error kk_accept(int sock, kk_context_t *ctx)
   }
 }
 
-
-
 // String must be borrowed?
-kk_std_core_exn__error kk_send(int sock, kk_string_t str, kk_context_t *ctx)
+kk_std_core_exn__error kk_string_send(const int sock, const kk_string_t str, kk_context_t *ctx)
 {
   const kk_bytes_t bytes = str.bytes;
   kk_ssize_t len = kk_bytes_len_borrow(bytes, ctx);
@@ -106,7 +117,7 @@ kk_std_core_exn__error kk_send(int sock, kk_string_t str, kk_context_t *ctx)
 }
 
 #define BUFFER_SIZE 1024 // Static buffer size for testing
-kk_string_t kk_recv(int sock, kk_context_t *ctx)
+kk_string_t kk_string_recv(const int sock, kk_context_t *ctx)
 {
   char buffer[BUFFER_SIZE];
   ssize_t received = recv(sock, (void *)buffer, BUFFER_SIZE, 0);
